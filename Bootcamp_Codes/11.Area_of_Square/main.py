9acacabdde2380e4e135dfeed2a190616b71985e
Ahmed Aboeita <ahmedmasre2233@gmail.com>
2024-09-29 18:35:18 +0300

    add ex of function

---
+++ Bootcamp_Codes/11.Area_of_Square/main.py
@@ -0,0 +1,13 @@
+# Define a function that calculates the area of a square. 
+# Area of square  = square side x square side
+
+# Example:
+# area_of_square(6) 
+# Output in console:
+# 36 
+
+def area_of_squre(square_side):
+    area=square_side**2
+    return area
+square_Side = 5
+print(area_of_squre(square_Side))
