4860db7b6d2ecd16037497b67af0aa0bdba401ea
Ahmed Aboeita <ahmedmasre2233@gmail.com>
2024-09-29 21:17:28 +0300

    add ex 12

---
+++ Bootcamp_Codes/12.Volume_Converter/main.py
@@ -0,0 +1,12 @@
+# Define a function that converts fluid ounces to milliliters and prints the result to the console.
+# Take into account that 1 fluid ounce is equal to 29.57353 milliliters.
+# Example:
+# volume_converter(5)
+# Output:
+# 147.86765 
+
+def volume_converter(num):
+    result = num*29.57353
+    print(result)
+
+print(volume_converter(10))
